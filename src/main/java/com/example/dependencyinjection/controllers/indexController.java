bfe2f4cd569b1875b3c8640cdcb86a2a2d72d847
DesiAPG <74121435+DesiAPG@users.noreply.github.com>
2023-02-14 12:22:41 -0500

    Using Autowired

--- src/main/java/com/example/dependencyinjection/controllers/indexController.java
+++ src/main/java/com/example/dependencyinjection/controllers/indexController.java
@@ -1,5 +1,6 @@
 package com.example.dependencyinjection.controllers;
 
+import com.example.dependencyinjection.model.service.IService;
 import com.example.dependencyinjection.model.service.MyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,12 +12,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("")
 public class indexController {
-    @Autowired
-    private MyService service;
+    private IService service;
+
+    //@Autowired
+    public indexController(IService service) {
+        this.service = service;
+    }
 
     @GetMapping(path = {"/", "/index", "/home", ""})
     public String index(Model model) {
         model.addAttribute("object", service.operation());
         return "index";
     }
+
 }
