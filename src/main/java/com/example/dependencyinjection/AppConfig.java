39527f0a8c7a88b45510e0062036cc0f024d8fbf
DesiAPG <74121435+DesiAPG@users.noreply.github.com>
2023-02-15 19:54:02 -0500

    Creating Beans to inject them in the facture

--- src/main/java/com/example/dependencyinjection/AppConfig.java
+++ src/main/java/com/example/dependencyinjection/AppConfig.java
@@ -1,11 +1,16 @@
 package com.example.dependencyinjection;
 
+import com.example.dependencyinjection.model.domain.FactureItem;
+import com.example.dependencyinjection.model.domain.Product;
 import com.example.dependencyinjection.model.service.IService;
 import com.example.dependencyinjection.model.service.MyService;
 import com.example.dependencyinjection.model.service.MyServiceComplex;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 public class AppConfig {
@@ -19,6 +24,16 @@ public class AppConfig {
     @Primary
     public IService registerServiceComplex() {
         return new MyServiceComplex();
+    }
+
+    @Bean("FactureItems")
+    public List<FactureItem> registerItem() {
+        Product product1 = new Product("Sony Camera", 100);
+        Product product2 = new Product("OnTrail Bicycle", 250);
+
+        FactureItem line1 = new FactureItem(product1, 2);
+        FactureItem line2 = new FactureItem(product2, 4);
+        return Arrays.asList(line1, line2);
     }
 
 }
