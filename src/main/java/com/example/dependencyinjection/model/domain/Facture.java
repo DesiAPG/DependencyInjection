920b4f79352b2145bd713977112be0cfc8a120cb
DesiAPG <74121435+DesiAPG@users.noreply.github.com>
2023-02-15 19:55:12 -0500

    Injecting the client and the items

---
+++ src/main/java/com/example/dependencyinjection/model/domain/Facture.java
@@ -0,0 +1,41 @@
+package com.example.dependencyinjection.model.domain;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class Facture {
+    @Value("${description.facture}")
+    private String description;
+    @Autowired
+    private Client client;
+    @Autowired
+    private List<FactureItem> items;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public List<FactureItem> getItems() {
+        return items;
+    }
+
+    public void setItems(List<FactureItem> items) {
+        this.items = items;
+    }
+}
