7a5b3472ce2fe3c016b26eaf3a3e93bb3e2c64f0
DesiAPG <74121435+DesiAPG@users.noreply.github.com>
2023-02-15 19:54:48 -0500

    Creating the attributes for client

---
+++ src/main/java/com/example/dependencyinjection/model/domain/Client.java
@@ -0,0 +1,28 @@
+package com.example.dependencyinjection.model.domain;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Client {
+    @Value("${client.name}")
+    private String name;
+    @Value("${client.surname}")
+    private String surname;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+}
