09e82c46bf7685dd1627338249451dbf82b5fd64
DesiAPG <74121435+DesiAPG@users.noreply.github.com>
2023-02-15 19:55:50 -0500

    Attributes about quantity and the product

---
+++ src/main/java/com/example/dependencyinjection/model/domain/FactureItem.java
@@ -0,0 +1,27 @@
+package com.example.dependencyinjection.model.domain;
+
+public class FactureItem {
+    private Product product;
+    private Integer quantity;
+
+    public FactureItem(Product product, Integer quantity) {
+        this.product = product;
+        this.quantity = quantity;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+}
