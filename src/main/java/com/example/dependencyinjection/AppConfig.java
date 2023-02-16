734a73c455453fe7a5386f81e7b02dc22dfe9a7d
DesiAPG <74121435+DesiAPG@users.noreply.github.com>
2023-02-16 10:19:14 -0500

    Created another bean to inject more products

--- src/main/java/com/example/dependencyinjection/AppConfig.java
+++ src/main/java/com/example/dependencyinjection/AppConfig.java
@@ -36,4 +36,18 @@ public class AppConfig {
         return Arrays.asList(line1, line2);
     }
 
+    @Bean("FactureItemsOffice")
+    public List<FactureItem> registerOfficeItem() {
+        Product product1 = new Product("LG Monitor", 100);
+        Product product2 = new Product("Office chair", 300);
+        Product product3 = new Product("Laptop", 1000);
+        Product product4 = new Product("Desktop", 300);
+
+        FactureItem line1 = new FactureItem(product1, 2);
+        FactureItem line2 = new FactureItem(product2, 4);
+        FactureItem line3 = new FactureItem(product3, 1);
+        FactureItem line4 = new FactureItem(product4, 3);
+        return Arrays.asList(line1, line2, line3, line4);
+    }
+
 }
